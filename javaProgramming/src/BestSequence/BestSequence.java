d2851e5285e42842212f730187c3d5fe51a54c91
adman1219 <seok_1219@hanmail.net>
2013-10-29 20:10:51 +0900

    commit

--- javaProgramming/src/BestSequence/BestSequence.java
+++ javaProgramming/src/BestSequence/BestSequence.java
@@ -4,13 +4,15 @@ public class BestSequence {
 	
 	private int[] comparisionArray = {1,2,3};
 	private int[] bestSequenceArray;
-	private int[][] bestSequencesArray;
 	private int count;
-	private int result = 0;
+	private int result;
+	private int sumCnt;
 	
 	public BestSequence(int[] bestSequenceArray, int count) {
 		this.bestSequenceArray = bestSequenceArray;
 		this.count = count;
+		this.sumCnt = 0;
+		this.result = 0;
 	}
 
 	public static void main(String[] args) {
@@ -21,10 +23,12 @@ public class BestSequence {
 	
 	private void findSequences(int index) {
 		if (index < this.count) {
-			for(int i = 0; i < this.comparisionArray.length; i++) {
-				if (this.validateSequenceValue(index-1, i)) {
-					this.bestSequenceArray[index] = this.comparisionArray[i];
+			for(int i = 1; i <= this.comparisionArray.length; i++) {
+				this.bestSequenceArray[index] = this.comparisionArray[i-1];
+				if (this.validateSequenceValue(index-1, index)) {
 					this.findSequences(index+1);
+				} else {
+					this.bestSequenceArray[index] = 0;
 				}
 			}
 		} else {
@@ -41,14 +45,50 @@ public class BestSequence {
 	
 	private boolean validateSequenceValue(int index, int curComparisionIndex) {
 		int counting = 0;
+		int success = 0;
+		int fail = 0;
+		int stopPoint = 0;
+		
 		if (index >= 0) {
-			while(counting <= curComparisionIndex) {
-				if (index-counting >= 0) {
-					if (this.comparisionArray[curComparisionIndex] == this.bestSequenceArray[index-counting]) {
-						return false;
+			while(counting < this.comparisionArray.length) {
+				if (index - counting >= 0) {
+					if (this.bestSequenceArray[curComparisionIndex] != this.bestSequenceArray[index-counting]) {
+						success++;
+						
+						if (curComparisionIndex < this.comparisionArray.length) {
+							break;
+						}
+					} else {
+						
+						if (counting == 0) {
+							return false;
+						}
+						
+						stopPoint = index-counting;
+						fail++;
+						break;
 					}
 				}
+				
 				counting++;
+			}
+			
+			if (curComparisionIndex <= this.comparisionArray.length) {
+				if (success == 0) {
+					return false;
+				} else {
+					if (stopPoint - success >= 0 ) {
+						return this.validateSequenceValue(stopPoint-1, curComparisionIndex-1);
+					}
+				}
+			} else {
+				if (fail == 0) {
+					return true;
+				} else {
+					if (stopPoint - success >= 0 ) {
+						return this.validateSequenceValue(stopPoint-1, curComparisionIndex-1);
+					}
+				}
 			}
 		}
 		return true;
