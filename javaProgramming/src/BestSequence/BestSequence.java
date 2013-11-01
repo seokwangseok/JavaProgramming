e9287967bf1a31f27e2580befa543a7af8c2ff1a
seokwangseok <seok_1219@hanmail.net>
2013-11-01 17:36:16 +0900

    complete

--- javaProgramming/src/BestSequence/BestSequence.java
+++ javaProgramming/src/BestSequence/BestSequence.java
@@ -5,95 +5,28 @@ public class BestSequence {
 	private int[] comparisionArray;
 	private int[] bestSequenceArray;
 	private int count;
+	private int totalCount;
 	
 	public BestSequence(int[] bestSequenceArray, int[] comparisionArray, int count) {
 		this.bestSequenceArray = bestSequenceArray;
 		this.comparisionArray = comparisionArray;
 		this.count = count;
+		this.totalCount = 0;
 	}
 
 	public static void main(String[] args) {
 		int n = 7;
 		BestSequence bestSequence = new BestSequence(new int[n], new int[1], n);
 		bestSequence.findSequence();
+		System.out.println("totalCount : " + bestSequence.totalCount);
 	}
-	
-	private void findSequences() {
-		
-//		if (index < this.count) {
-//			for(int i = 1; i <= this.comparisionArray.length; i++) {
-//				this.bestSequenceArray[index] = this.comparisionArray[i-1];
-//				if (this.validateSequenceValue(index-1, index)) {
-//					this.findSequences(index+1);
-//				} else {
-//					this.bestSequenceArray[index] = 0;
-//				}
-//			}
-//		} else {
-//			for(int j = 0; j < this.bestSequenceArray.length; j++) {
-//				System.out.print(this.bestSequenceArray[j] + "  ");
-//			}
-//			System.out.println();
-//		}
-	}
-	
-//	private boolean validateSequenceValue(int index, int curComparisionIndex) {
-//		int counting = 0;
-//		int success = 0;
-//		int fail = 0;
-//		int stopPoint = 0;
-//		
-//		if (index >= 0) {
-//			while(counting < this.comparisionArray.length) {
-//				if (index - counting >= 0) {
-//					if (this.bestSequenceArray[curComparisionIndex] != this.bestSequenceArray[index-counting]) {
-//						success++;
-//						
-//						if (curComparisionIndex < this.comparisionArray.length) {
-//							break;
-//						}
-//					} else {
-//						
-//						if (counting == 0) {
-//							return false;
-//						}
-//						
-//						stopPoint = index-counting;
-//						fail++;
-//						break;
-//					}
-//				}
-//				
-//				counting++;
-//			}
-//			
-//			if (curComparisionIndex <= this.comparisionArray.length) {
-//				if (success == 0) {
-//					return false;
-//				} else {
-//					if (stopPoint - success >= 0 ) {
-//						return this.validateSequenceValue(stopPoint-1, curComparisionIndex-1);
-//					}
-//				}
-//			} else {
-//				if (fail == 0) {
-//					return true;
-//				} else {
-//					if (stopPoint - success >= 0 ) {
-//						return this.validateSequenceValue(stopPoint-1, curComparisionIndex-1);
-//					}
-//				}
-//			}
-//		}
-//		return true;
-//	}
 	
 	private void findSequence() {
 		int[] inputArray = {1,2,3};
 		for(int i=0; i < inputArray.length; i++) {
 			this.comparisionArray = new int[1];
 			this.comparisionArray[0] = i+1;
-			rotateArray(i);
+			rotateArray(0);
 		}
 	}
 	
@@ -105,14 +38,7 @@ public class BestSequence {
 			for(int i : inputArray) {
 				this.comparisionArray[0] = i;
 				if (this.isValidValue(index, this.bestSequenceArray, this.comparisionArray)) {
-//					this.bestSequenceArray[index] = this.comparisionArray[this.comparisionArray.length-1];
-					
-//					this.comparisionArray[0] = i;
 					this.rotateArray(index+1);
-				} else {
-//					index++;
-//					this.comparisionArray = new int[1];
-//					this.comparisionArray[0] = i;
 				}
 			}
 		} else {
@@ -121,6 +47,7 @@ public class BestSequence {
 				System.out.print("  ");
 			}
 			System.out.println();
+			this.totalCount++;
 		}
 	}
 	
@@ -128,15 +55,9 @@ public class BestSequence {
 		if (compareArray.length <= this.count/2) {
 			int count = 0;
 			for(int i = 0; i < compareArray.length; i++) {
-//				if (index == 0 || compareArray.length > 1) {
-					if (originArray[index+i] == compareArray[i]) {
-						count++;
-					}
-//				} else {
-//					if (originArray[i-index] == compareArray[i-1]) {
-//						count++;
-//					}
-//				}
+				if (originArray[index+i] == compareArray[i]) {
+					count++;
+				}
 			}
 			
 			if (count == compareArray.length) {
@@ -147,28 +68,24 @@ public class BestSequence {
 	}
 	
 	private boolean isValidValue(int index, int[] originArray, int[] compareArray) {
+		// 더이상 조사할게 없을 때 
+		if (index < 0 || compareArray.length > originArray.length/2) {
+			this.comparisionArray = compareArray;
+			return true;
+		}
+		
 		if (this.isEquivalentArray(index, originArray, compareArray)) {
 			this.comparisionArray = new int[1];
 			return false;
 		}
 		
-		if (compareArray.length >= originArray.length/2) {
-			this.comparisionArray = compareArray;
-			return true;
-		}
-		
-		// 여기 조건이 잘못됨 
-		if (compareArray.length == 1 && index < 2) {
-			this.comparisionArray = compareArray;
-			return true;
-		}
-		
-//		compareArray = new int[compareArray.length+1];
-		int[] tempArray = this.insertFirstChildInArray(originArray[index], compareArray);
+		// 이거 수정해야함 
+		int value = (compareArray.length > 1) ? originArray[index+1] : originArray[index];
+		int[] tempArray = this.insertFirstChildInArray(value, compareArray);
 		compareArray = new int[tempArray.length];
 		compareArray = tempArray;
-		return this.isValidValue(index-compareArray.length, originArray, compareArray);
 		
+		return this.isValidValue(index-2, originArray, compareArray);
 	}
 	
 	private int[] insertFirstChildInArray(int value, int[] compareArray) {
